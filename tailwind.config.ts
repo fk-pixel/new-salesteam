962c8c621215058a4d27a6eeb2fe275fba62030f
Fatih Kurt <fthbtlkrt@gmail.com>
2024-11-16 00:52:43 +0100

    add new tailwind configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,13 +5,12 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        cugray: "#808080",
       },
     },
   },
