4018d5bfb0493c2ec857810431596bd97371f389
Fatih Kurt <fthbtlkrt@gmail.com>
2024-11-17 00:13:30 +0100

    add portal

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,12 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import Portal from "@/components/Portal";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Sales-Team APP",
+  title: "Sales-Team",
 };
 
 export default function RootLayout({
@@ -15,7 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="mx-1">
+          <Portal>{children}</Portal>
+        </main>
+      </body>
     </html>
   );
 }
