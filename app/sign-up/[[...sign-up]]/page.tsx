aa03c8e5573604c01e33f642fa4cc37bbf877840
Fatih Kurt <fthbtlkrt@gmail.com>
2024-11-25 00:49:14 +0100

    remove unused side of sing-up pge

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -6,22 +6,6 @@ import { SignUp } from '@clerk/nextjs'
 
 
 export default function SignUpPage() {
-  type FieldType = {
-    username?: String;
-    email?: string;
-    password?: string;
-  };
-
-  const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
-    console.log("Success:", values);
-  };
-
-  const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (
-    errorInfo
-  ) => {
-    console.log("Failed:", errorInfo);
-  };
-
   return (
     <>
        <div className="flex justify-center items-center min-h-screen bg-gray-100">
