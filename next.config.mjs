b8ef46b71ae06c09753503a9925ee3743cd1ca7e
Fatih Kurt <fthbtlkrt@gmail.com>
2024-12-02 23:06:34 +0100

    add new config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+        pathname: '**',
+      },
+    ],
+  },
+  // async headers() {
+  //     return [
+  //         {
+  //             source: '/:path*',
+  //             headers: [
+  //                 {
+  //                     key: 'X-Frame-Options',
+  //                     value: 'SAMEORIGIN',
+  //                 },
+  //                 {
+  //                     key: 'Content-Security-Policy',
+  //                     value: `frame-ancestors 'self' https://app.contentful.com;`,
+  //                 },
+  //             ],
+  //         },
+  //     ];
+  // },
+};
 
 export default nextConfig;
